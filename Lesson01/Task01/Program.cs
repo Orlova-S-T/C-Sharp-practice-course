e6bdc67392347a75c8c49eb6d7b5e6ed4b98910a
Svetlana Orlova <ptenchich@gmail.com>
2022-12-15 14:07:53 +0300

    Added Task 01

---
+++ Lesson01/Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int square = secondNumber * secondNumber;
+
+if (square == firstNumber)
+{
+    Console.Write("Второе число является квадратом первого");
+}
+
+else
+{
+    Console.Write("Второе число не является квадратом первого");
+}
+
