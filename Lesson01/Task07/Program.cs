89e94a69cb6eb53826a36161edd190f8f107550b
Svetlana Orlova <ptenchich@gmail.com>
2022-12-16 17:17:06 +0300

    Added Task 07

---
+++ Lesson01/Task07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int module = Math.Abs(number);
+if (module > 99 && module < 1000)
+{
+    int lastDigit = module % 10;
+    Console.WriteLine(lastDigit);
+}
+else
+Console.WriteLine("Неправильно набран номер");
