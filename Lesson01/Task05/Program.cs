61e0153be64997281be72d29f7de1c346d261583
Svetlana Orlova <ptenchich@gmail.com>
2022-12-16 17:02:29 +0300

    Added Task 05

---
+++ Lesson01/Task05/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int module = Math.Abs(number);
+
+int count = - module;
+
+while (count < module)
+{
+    Console.Write($"{count}, ");
+    count++;
+}
+Console.WriteLine(count);
+
