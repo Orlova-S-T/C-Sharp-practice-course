7169fea0135c8e80ebf2403fe557486927acc55e
Svetlana Orlova <ptenchich@gmail.com>
2022-12-16 18:46:45 +0300

    Added Task 06

---
+++ Lesson01/Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Введённое число {number} является чётным");
+}
+
+else
+{
+    Console.WriteLine($"Введённое число {number} не является чётным");
+}
