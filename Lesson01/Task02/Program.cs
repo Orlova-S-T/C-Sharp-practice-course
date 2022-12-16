4d71c22198cdb297c0a597d7b11874f23d57bdb7
Svetlana Orlova <ptenchich@gmail.com>
2022-12-16 18:14:33 +0300

    Added Task 02

---
+++ Lesson01/Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое целое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"{firstNumber} больше чем {secondNumber}");
+}
+else if (firstNumber == secondNumber)
+{
+    Console.WriteLine("Среди введённых чисел нет большего и меньшего - эти числа равны");
+}
+else
+{
+    Console.WriteLine($"{secondNumber} больше чем {firstNumber}");
+}
+
