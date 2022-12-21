f8db87276d917addc92678f294efadec289e9173
Svetlana Orlova <ptenchich@gmail.com>
2022-12-21 15:05:51 +0300

    Added Task 11

---
+++ Lesson02/Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int DelMiddleDigit(int num) //Параметр num в нашем случае будет равен локальной переменной number.
+{
+    int firstDigit = num / 100; //Первая цифра трёхзначного числа.
+    int thirdDigit = num % 10;  //Последняя цифра трёхзначного числа.
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+
+int number = new Random().Next(100, 1000);
+Console.Write($"Случайное число из диапазона [100, 999] равно {number} -> искомое число равно ");
+int digit = DelMiddleDigit(number); //Искомое двузначное число.
+Console.WriteLine(digit);
+
