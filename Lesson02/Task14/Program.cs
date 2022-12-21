b3b531247b8696d6a69f6a95b5689332dedc2af7
Svetlana Orlova <ptenchich@gmail.com>
2022-12-21 16:57:47 +0300

    Added Task 14

---
+++ Lesson02/Task14/Program.cs
@@ -0,0 +1,11 @@
+﻿bool Div7And23Check(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+Console.Write("Введите целое число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+if (Div7And23Check(userNumber1)) Console.WriteLine($"{userNumber1} кратно одновременно 7 и 23");
+else Console.WriteLine($"{userNumber1} не кратно одновременно 7 и 23");
+
