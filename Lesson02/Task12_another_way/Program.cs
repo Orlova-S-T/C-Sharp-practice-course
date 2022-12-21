c6ed4e81dc5abeee54f4f35d88d15f949349d9dc
Svetlana Orlova <ptenchich@gmail.com>
2022-12-21 15:21:39 +0300

    Added Task 12 (another way)

---
+++ Lesson02/Task12_another_way/Program.cs
@@ -0,0 +1,20 @@
+﻿bool DivCheck(int num1, int num2)
+{
+    return num1 % num2 == 0;
+}
+
+Console.Write("Введите первое число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+if (DivCheck(userNumber1, userNumber2)) //Такой вывод означает "если в функции получилось true".
+{
+    Console.WriteLine($"{userNumber1}, {userNumber2} -> кратно");
+}
+else
+{
+    Console.WriteLine($"{userNumber1}, {userNumber2} -> не кратно, остаток " + (userNumber1 % userNumber2));
+}
+
