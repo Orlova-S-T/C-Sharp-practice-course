957cb3c80dbe0bcea07e8a431ee09ab52fc29276
Svetlana Orlova <ptenchich@gmail.com>
2022-12-21 18:00:23 +0300

    Added Task 16

---
+++ Lesson02/Task16/Program.cs
@@ -0,0 +1,16 @@
+﻿bool SquareCheck(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
+
+
+Console.Write("Введите первое целое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+if (SquareCheck(firstNumber, secondNumber)) Console.WriteLine($"{firstNumber}, {secondNumber} -> одно число является квадратом другого");
+else Console.WriteLine($"{firstNumber}, {secondNumber} -> ни одно из этих двух чисел не является квадратом другого");
+
