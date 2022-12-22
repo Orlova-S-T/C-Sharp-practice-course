a0064c4f078b8f3ed9b2bfe5e69f92d432291445
Svetlana Orlova <ptenchich@gmail.com>
2022-12-22 12:58:23 +0300

    Added Task 10

---
+++ Lesson02/Task10/Program.cs
@@ -0,0 +1,26 @@
+﻿bool CheckThreeDigit(int num) //Метод проверки целого числа на трёхзначность.
+{
+    return Math.Abs(num) > 99 && Math.Abs(num) < 1000; //В условии задачи не сказано, что трёхзначное число должно быть положительным.
+}
+
+int MidFromTree(int fig) //Метод, вычисляющий среднюю цифру целого трёхзначного числа.
+{
+    int result = fig % 100 / 10;
+    return Math.Abs(result);
+}
+
+
+Console.Write("Введите целое трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (!CheckThreeDigit(number))
+{
+    Console.WriteLine($"Введённое вами число {number} не является трёхзначным. Перезапустите программу и повторите попытку.");
+}
+
+else
+{
+    int figure = MidFromTree(number);
+    Console.WriteLine($"{number} -> {figure}");
+}
+
