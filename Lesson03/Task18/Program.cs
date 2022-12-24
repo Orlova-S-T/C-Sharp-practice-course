2cd0af8a3953610a28d91d3cfbc8aa3fde29d4ce
Svetlana Orlova <ptenchich@gmail.com>
2022-12-24 18:12:56 +0300

    Added Task 18

---
+++ Lesson03/Task18/Program.cs
@@ -0,0 +1,19 @@
+﻿string Range(int quart)
+{
+    if (quart == 1) return "X > 0, Y > 0";
+    if (quart == 2) return "X < 0, Y > 0";
+    if (quart == 3) return "X < 0, Y < 0";
+    if (quart == 4) return "X > 0, Y < 0";
+    return "Ошибка ввода. Номер координатной четверти должен быть целым числом от 1 до 4.";
+}
+
+
+Console.Write("Введите номер четверти координатной плоскости от 1 до 4: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string result = Range(quarter);
+
+Console.WriteLine(result);
+
+
+
