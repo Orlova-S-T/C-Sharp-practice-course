d5583861be271e958d76b4c9f8a6e32b9bc2bffd
Svetlana Orlova <ptenchich@gmail.com>
2022-12-24 17:20:15 +0300

    Added Task 17

---
+++ Lesson03/Task17/Program.cs
@@ -0,0 +1,24 @@
+﻿int Quarter(int X, int Y)
+{
+    if (X > 0 && Y > 0) return 1;
+    if (X < 0 && Y > 0) return 2;
+    if (X < 0 && Y < 0) return 3;
+    if (X > 0 && Y < 0) return 4;
+    return 0;
+}
+
+
+Console.WriteLine("Введите координаты точки (не равные нулю)");
+Console.Write("x = ");
+int userX = Convert.ToInt32(Console.ReadLine());
+Console.Write("y = ");
+int userY = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(userX, userY);
+
+string result = quarter > 0
+        ? $"Точка с координатами x = {userX}, y = {userY} принадлежит {quarter} четверти координатной плоскости."
+        : "Ошибка ввода. Координаты точки не должны быть равны 0.";
+//Для лучшей читаемости тернарного оператора можно делать переносы для отдельных строк.
+
+Console.WriteLine(result);
