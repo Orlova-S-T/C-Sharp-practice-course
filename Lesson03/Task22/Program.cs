db8ae2fdb54dbd794e7285bf177dbbab2e7c08b5
Svetlana Orlova <ptenchich@gmail.com>
2022-12-25 17:26:01 +0300

    Added Task 22

---
+++ Lesson03/Task22/Program.cs
@@ -0,0 +1,48 @@
+﻿int UserInput(string text1, string text2)
+{
+    Console.WriteLine(text1);
+    Console.Write(text2);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+bool NaturalSetCheck(int num)
+{
+    return num > 0;
+}
+
+void SquareN(int naturalNumber)
+{
+    double squareN = Math.Pow(naturalNumber, 2);
+    Console.WriteLine($"{naturalNumber} -> {squareN}");
+    // Здесь можно вывести результат по-другому:
+    // Console.WriteLine($"{naturalNumber,5} {squareN,5}");
+    // 5 - количество символов, задействованных для вывода переменной,
+    // тогда получим ровные столбцы для числа знаков <= 5.
+}
+
+
+int number = UserInput("Введите любое натуральное число", "N = ");
+
+if (NaturalSetCheck(number))
+{
+    int count = 1;
+    while (count <= number)
+    {
+        SquareN(count);
+        count++;
+    }
+}
+
+else Console.Write($"Ошибка ввода. Число {number} не является действительным. Необходимо ввести целое число больше ноля. Снова запустите программу и повторите попытку.");
+
+
+// Для проверки числа на натуральность можно было использовать цикл:
+// int number = UserInput("Введите любое натуральное число", "N = ");
+// while (number < 1)
+// {
+//     number = UserInput("Введите любое натуральное число", "N = ");
+// }
+// Тогда пользователя зацикли ли бы на ввод правильного числа.
+// Дальше пишем программу для натурального числа.
+
