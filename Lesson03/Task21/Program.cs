4297d5199181fc129fa987a4d5cdf72d9d109955
Svetlana Orlova <ptenchich@gmail.com>
2022-12-25 15:05:12 +0300

    Added Task 21

---
+++ Lesson03/Task21/Program.cs
@@ -0,0 +1,27 @@
+﻿double UserInput(string text1, string text2)
+{
+    Console.WriteLine(text1);
+    Console.Write(text2);
+    double input = Convert.ToDouble(Console.ReadLine());
+    return input;
+}
+
+double DistABin3D(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    double distanceAB = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2));
+    return Math.Round(distanceAB, 2, MidpointRounding.ToZero);
+}
+
+
+double x1 = UserInput("Введите координату Х первой точки", "x1 = ");
+double y1 = UserInput("Введите координату Y первой точки", "y1 = ");
+double z1 = UserInput("Введите координату Z первой точки", "z1 = ");
+
+double x2 = UserInput("Введите координату Х второй точки", "x2 = ");
+double y2 = UserInput("Введите координату Y второй точки", "y2 = ");
+double z2 = UserInput("Введите координату Z второй точки", "z2 = ");
+
+
+double result = DistABin3D(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Расстояние между точками с координатами ({x1}, {y1}, {z1}) и ({x2}, {y2}, {z2}) равно {result}");
+
