360c8e8179f5bd1ef7d02a087dc9c9ca374e7085
Svetlana Orlova <ptenchich@gmail.com>
2022-12-25 18:22:00 +0300

    Added Task 23

---
+++ Lesson03/Task23/Program.cs
@@ -0,0 +1,34 @@
+﻿int UserInput(string text1, string text2)
+{
+    Console.WriteLine(text1);
+    Console.Write(text2);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+bool NaturalSetCheck(int num)
+{
+    return num > 0;
+}
+
+void CubeN(int naturalNumber)
+{
+    double cubeN = Math.Pow(naturalNumber, 3);
+    Console.WriteLine($"{naturalNumber,5} | {cubeN,10}");
+}
+
+void PlusOne(int lastInRange)
+{
+    int count = 1;
+    while (count <= lastInRange)
+    {
+        CubeN(count);
+        count++;
+    }
+}
+
+int number = UserInput("Введите любое натуральное число", "N = ");
+
+if (NaturalSetCheck(number)) PlusOne(number);
+else Console.Write($"Ошибка ввода. Число {number} не является действительным. Необходимо ввести целое число больше ноля. Снова запустите программу и повторите попытку.");
+
