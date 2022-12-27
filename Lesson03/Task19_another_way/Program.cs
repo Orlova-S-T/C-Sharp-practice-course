af0a46ff27ac52aec4d4f16892510d3f3cd90a77
Svetlana Orlova <ptenchich@gmail.com>
2022-12-27 16:17:38 +0300

    Added Task 19 another way

---
+++ Lesson03/Task19_another_way/Program.cs
@@ -0,0 +1,34 @@
+﻿int UserInput(string text) //Ввод пользователем числа.
+{
+    Console.Write(text);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+
+int RevNum(int num) // "Переворот" числа.
+{
+    int revNum = 0;
+    while (num > 0)
+    {
+        revNum = revNum * 10 + num % 10;
+        num = num / 10;
+    }
+    return revNum;
+}
+
+
+int userNumber = UserInput("Введите пятизначное целое число: ");
+int number = Math.Abs(userNumber);
+if (number > 9999 && number < 100000) //Конкретно в нашей задаче - число должно быть пятизначным.
+{
+    int revNum = RevNum(number);
+    Console.WriteLine($"{number} -> {revNum}");
+    if (number == revNum) Console.WriteLine($"Число {userNumber} является палиндромом");
+    else Console.WriteLine($"Число {userNumber} не является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Ошибка ввода. Введённое вами число не является пятизначным. Снова запустите программу и повторите попытку");
+}
+
