6cee64adc91fa52490febef20e864fece0c977a2
Svetlana Orlova <ptenchich@gmail.com>
2023-01-12 17:15:14 +0300

    Added Task 26

---
+++ Lesson04/Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе:
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int DigitCount(int num)
+{
+    int count = 0;
+    //while (num > 0) Не работает для отрицательных чисел и 0.
+    while (num != 0) // Не работает только для 0 - для него выдаёт 0 цифр.
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = DigitCount(number);
+Console.WriteLine($"Количество цифр в числе {number} равно {result}");
+
