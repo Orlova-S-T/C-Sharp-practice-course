046854ce75544af568f6abbb6e32d6ffbd441c0d
Svetlana Orlova <ptenchich@gmail.com>
2023-01-13 11:09:31 +0300

    Added Task 30 another way

---
+++ Lesson04/Task30_another_way/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке:
+// [1,0,1,1,0,1,0,0]
+
+// Другой вариант:
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random(); // Создаём отдельную переменную, иначе может быть генерация
+                               // одинаковых псевдослучайных чисел.
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+int[] arr = CreateArray(8);
+PrintArray(arr);
+
