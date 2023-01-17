383e53e9c40036267b2cf843af33d22da5b6b994
Svetlana Orlova <ptenchich@gmail.com>
2023-01-17 11:43:35 +0300

    Added Task 32

---
+++ Lesson05/Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 32: Напишите программу замены элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+int[] CreateArrayRndInt(int size, int min, int max) // Создание массива и заполнение его случайными чмслами.
+{
+    int[] collection = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = rnd.Next(min, max + 1);
+    }
+    return collection;
+}
+
+void PrintArray(int[] collection) // Вывод массива на печать с квадратными скобками.
+{
+    System.Console.Write("[");
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (i < collection.Length - 1) System.Console.Write(collection[i] + ", ");
+        else System.Console.Write(collection[i]);
+    }
+    System.Console.WriteLine("]");
+}
+
+void ReversSignElem(int[] collection) // Изменение знаков элементов массива на противоположные.
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = -collection[i];
+    }
+}
+
+int[] array = CreateArrayRndInt(4, -10, 10);
+PrintArray(array);
+
+ReversSignElem(array);
+PrintArray(array);
+
