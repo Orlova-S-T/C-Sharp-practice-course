3ae7090c9a868ca8ea5c295cb335f014eda0a248
Svetlana Orlova <ptenchich@gmail.com>
2023-01-20 18:21:46 +0300

    Added Task 42 one more

---
+++ Lesson06/Task42_one_more/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+// Моё решение с "нелегальными" встроенными методами:
+int UserInput(string text) // Ввод пользователем числа int.
+{
+    Console.Write(text);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+
+string GetRemaindersOfDivisionByTwo(int num, string txt) // Последовательное нахождение остатков деления десятичного числа на 2 (с рекурсией).
+{
+    txt = txt + (num % 2);
+    if (num / 2 != 0) txt = GetRemaindersOfDivisionByTwo(num / 2, txt);
+    return txt;
+}
+
+
+int decimalNumber = UserInput("Введите любое натуральное число: ");
+
+string revNum = GetRemaindersOfDivisionByTwo(decimalNumber, "");
+Console.WriteLine(revNum);
+
+char [] array = revNum.ToCharArray();
+Array.Reverse(array);
+string binNum = new String(array);
+int binaryNumber = Convert.ToInt32(binNum);
+
+Console.WriteLine($"Десятичное число {decimalNumber} в двоичной системе счисления равно {binaryNumber}");
+
